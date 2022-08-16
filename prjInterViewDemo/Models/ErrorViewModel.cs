dbe7e753b1c145f855acbf54369ec5ed8581f3a5
zgenez <yaj4604123@gmail.com>
2022-08-16 18:35:12 +0800

    加入專案檔案。

---
+++ prjInterViewDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prjInterViewDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
